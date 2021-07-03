bfca150e442f0b3f90df2a8fc0ad8dd0fa004806
Ivo Leitão <ivo.leitao@sky.uk>
2021-07-03 18:44:10 +0100

    Moved gitignore

---
+++ packages/shadertoy_client/example/env.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnvifyGenerator
+// **************************************************************************
+
+class _Env {
+  static const String apiKey = 'NtnKM7';
+  static const String user = 'shaderflix';
+  static const String password = '237zCzwQrnm6Z4';
+}
