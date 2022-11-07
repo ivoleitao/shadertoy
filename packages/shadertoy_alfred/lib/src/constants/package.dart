2fb92c86d90e4438e14d2fd009b4466bc03b734f
Ivo Leitão <ivo.leitao@gmail.com>
2022-11-07 11:18:29 +0000

    chore: updated version

--- packages/shadertoy_alfred/lib/src/constants/package.dart
+++ packages/shadertoy_alfred/lib/src/constants/package.dart
@@ -3,7 +3,7 @@
 import 'package:pubspec_parse/pubspec_parse.dart';
 
 final pubspec = Pubspec.parse("""name: shadertoy_alfred
-version: 2.2.2
+version: 2.3.3
 description: >-
   Shadertoy alfred support
 publish_to: none
@@ -20,11 +20,12 @@ dependencies:
   path: ^1.8.2
   # Other dependencies
   logger: ^1.1.0
+  collection: ^1.17.0
   args: ^2.3.1
   pubspec_parse: ^1.2.1
-  alfred_workflow: ^0.2.7
+  alfred_workflow: ^0.2.10
   shadertoy: ^2.3.0
-  shadertoy_client: ^2.3.0
+  shadertoy_client: ^2.4.0
 dev_dependencies:
   lints: ^2.0.1
   build_runner: ^2.3.2
